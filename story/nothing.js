58283b08d8b8c2b6ef6fdd8fca1679155dd89a49
v0x76 <v0x76@users.noreply.github.com>
2018-08-18 18:29:41 +0100

    New button design + more story

--- story/nothing.js
+++ story/nothing.js
@@ -36,7 +36,51 @@ var Story = {
                 lines: [ '...' ],
                 buttons: [{
                     text: 'Start',
-                    onclick: ()=> { Game.setPage( this.nothing.ii, 1000 ) }
+                    onclick: ()=> { Game.setPage( this.nothing.epigraph, 1000 ) }
+                }]
+            }
+        },
+
+        epigraph: function() {
+            return {
+                lines: [ 'Unbeaten by the rain',
+                         'Unbeaten by the wind',
+                         'Nor the snow, nor summer heat.' ],
+                buttons: [{
+                    text: 'Become',
+                    onclick: ()=> {
+                        Game.setPage( this.nothing.prologue, 1800 )
+                        $(':root').css('--primary-color', 'bisque')
+                        $(':root').css('--bg-color', 'royalblue')
+                    }
+                }]
+            }
+        },
+
+        prologue: function() {
+            return {
+                lines: [ 'In the city centre, sitting at the side',
+                         'of a small water fountain surrounded by oak trees,',
+                         'I sipped my coffee.' ],
+                buttons: [{
+                    text: 'Collect',
+                    onclick: ()=> { Game.setPage( this.nothing.prologue_ii, 1000 ) }
+                }]
+            }
+        },
+
+        prologue_ii: function() {
+            return {
+                lines: [ 'After taking a few moments',
+                         'to collect my thoughts,',
+                         'I reread the report.' ],
+                buttons: [{
+                    text: 'Read',
+                    onclick: ()=> {
+                        Game.setPage( this.nothing.ii, 1000 )
+                        $(':root').css('--primary-color', 'peachpuff')
+                        $(':root').css('--bg-color', 'midnightblue')
+                    }
                 }]
             }
         },
@@ -49,11 +93,7 @@ var Story = {
                     onclick: ()=> { Game.setPage( this.nothing.ii_a, 1000 ) }
                 }, {
                     text: 'Get up',
-                    onclick: ()=> {
-                        Game.setPage( this.nothing.iii, 1000 )
-                        $(':root').css('--primary-color', 'lightskyblue')
-                        $(':root').css('--bg-color', 'aliceblue')
-                    }
+                    onclick: ()=> { Game.setPage( this.nothing.iii, 1000 ) }
                 }]
             }
         },
@@ -63,11 +103,7 @@ var Story = {
                 lines: [ 'A waking function.', 'Internal.' ],
                 buttons: [{
                     text: 'Get up',
-                    onclick: ()=> {
-                        Game.setPage( this.nothing.iii, 1000 )
-                        $(':root').css('--primary-color', 'deepskyblue')
-                        $(':root').css('--bg-color', 'aliceblue')
-                    }
+                    onclick: ()=> { Game.setPage( this.nothing.iii, 1000 ) }
                 }]
             }
         },
@@ -77,14 +113,31 @@ var Story = {
                 lines: [ 'Work had called,', 'so he got himself ready', 'and left home.' ],
                 buttons: [{
                     text: 'Leave',
-                    onclick: ()=> { Game.setPage( this.nothing.iv, 1000 ) }
+                    onclick: ()=> { Game.setPage( this.nothing.iv, 1000 )
+                        $(':root').css('--primary-color', 'deepskyblue')
+                        $(':root').css('--bg-color', 'aliceblue')
+                        $('body').css('background-image', `
+                            radial-gradient(circle at 50% 0,
+                                rgba(255,248,97,.16),
+                                rgba(255,248,97,0) 79%),
+
+                            radial-gradient(circle at 9% 81%,
+                                rgba(119,214,255,.16),
+                                rgba(119,214,255,0) 79%),
+
+                            radial-gradient(circle at 100% 70%,
+                                rgba(204,83,103,.16),
+                                rgba(204,83,103,0) 79%)
+                        `)
+                    }
                 }]
             }
         },
 
         iv: function() {
             return {
-                lines: [ 'He was happy to find', 'it was snowing.' ],
+                lines: [ 'He was happy to find', 'it was snowing.',
+                         'A sense of calm took him', 'as he started to jog.' ],
                 buttons: [{
                     text: 'Jog',
                     onclick: ()=> { Game.setPage( this.nothing.v, 1000 ) }
@@ -94,43 +147,66 @@ var Story = {
 
         v: function() {
             return {
-                lines: [ 'Like always, he would jog to work.', 'Though he still felt drowsy', 'after sleeping so long.' ],
+                lines: [ 'He always travelled to work this way.',
+                         'It was efficient for sure', '—James didn\'t tire so easily—',
+                         'but perhaps he enjoyed the act itself.' ],
                 buttons: [{
-                    text: 'Scenic route',
-                    onclick: ()=> { Game.setPage( this.nothing.v_a, 1000 ) }
-                }, {
-                    text: 'Fast route',
-                    onclick: ()=> { Game.setPage( this.nothing.v_b, 1000 ) }
+                    text: 'To jog',
+                    onclick: ()=> { Game.setPage( this.nothing.vi, 1000 ) }
                 }]
             }
         },
 
-        v_a: function() {
+        vi: function() {
+            return {
+                lines: [ 'The constant process', 'of placing one leg in front of the other,',
+                         'tasting the cold air as it breezes by.' ],
+                buttons: [{
+                    text: 'Continue',
+                    onclick: ()=> { Game.setPage( this.nothing.vii, 1000 ) }
+                }]
+            }
+        },
+
+        vii: function() {
+            return {
+                lines: [ 'Though he still felt drowsy', 'after sleeping so long.' ],
+                buttons: [{
+                    text: 'Scenic route',
+                    onclick: ()=> { Game.setPage( this.nothing.vii_a, 1000 ) }
+                }, {
+                    text: 'Fast route',
+                    onclick: ()=> { Game.setPage( this.nothing.vii_b, 1000 ) }
+                }]
+            }
+        },
+
+        vii_a: function() {
             return {
                 lines: [ 'So he decided to take the scenic route.' ],
                 buttons: [{
-                    text: 'Mindfulness',
-                    onclick: ()=> { Game.setPage( this.nothing.v_a_i, 1000 ) }
+                    text: 'Mindful',
+                    onclick: ()=> { Game.setPage( this.nothing.vii_a_i, 1000 ) }
                 }]
             }
         },
 
-        v_b: function() {
+        vii_b: function() {
             return {
                 lines: [ 'So he decided to get straight to work.' ],
                 buttons: [{
                     text: 'Work',
-                    onclick: ()=> { Game.setPage( this.nothing.v_b, 1000 ) }
+                    onclick: ()=> { Game.setPage( this.nothing.vii_b, 1000 ) }
                 }]
             }
         },
 
-        v_a_i: function() {
+        vii_a_i: function() {
             return {
                 lines: [ 'Taking in the scenery', 'was one of James\' greatest joys.' ],
                 buttons: [{
                     text: 'Feel',
-                    onclick: ()=> { Game.setPage( this.nothing.v_a_i, 1000 ) }
+                    onclick: ()=> { Game.setPage( this.nothing.vii_a_i, 1000 ) }
                 }]
             }
         }
